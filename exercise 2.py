23930cba8c2942f1a812f1ae9fdc109a26c17706
Shaban <shabanxhemajli@live.com>
2022-08-10 10:00:39 +0200

    first commit

---
+++ exercise 2.py
@@ -0,0 +1,10 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+print("Is the addition of two numbers: ", (2+2))
+print("Is the substraction of two numbers: ", (10-3))
+print("Is the multiplication of two numbers: ", (4*2))
+print("Is the division of two numbers: ", (4/3))
+print("Is the modulo of two numbers: ", (8%+3))
+print("Is the power of a number: ", (4**2))
+print("Is the floor rounding of two numbers: ", (16//6))
