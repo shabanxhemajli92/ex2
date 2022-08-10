59fb023e217b0bcb5b12b9b165739d093e45284e
Shaban <shabanxhemajli@live.com>
2022-08-10 10:35:09 +0200

    ss

--- exercise 2.py
+++ exercise 2.py
@@ -1,6 +1,7 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
+#Task 1
 print("Is the addition of two numbers: ", (2+2))
 print("Is the substraction of two numbers: ", (10-3))
 print("Is the multiplication of two numbers: ", (4*2))
@@ -8,3 +9,13 @@ print("Is the division of two numbers: ", (4/3))
 print("Is the modulo of two numbers: ", (8%+3))
 print("Is the power of a number: ", (4**2))
 print("Is the floor rounding of two numbers: ", (16//6))
+#this is not a task its just trying the lists out 
+from math import*
+days_totals=[600,100,300,1000,1200,456,664,1486]
+print("Is the maximum value for the days totals",max(days_totals))
+print("Is the minimum value for the days totals",min(days_totals))
+#Task 2
+absolute_value=(-2)
+print("the absloute value of the numbers above is:",abs(absolute_value))#3
+
+
